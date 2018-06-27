eb2f64609db5b32a94855da13da90a48ea5e1f1f
DaanvandenBergh <DaanvandenBergh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-06-27 12:16:31 +0000

    First release
    git-svn-id: https://plugins.svn.wordpress.org/host-webfonts-local/trunk@1899945 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/welcome-panel.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package: CAOS for Webfonts
+ * @author: Daan van den Bergh
+ * @copyright: (c) 2018 Daan van den Bergh
+ * @url: https://dev.daanvandenbergh.com
+ */
+?>
+<div id="welcome-panel" class="welcome-panel">
+	<div class="welcome-panel-content">
+		<h2>Thank you for using CAOS for Webfonts!</h2>
+		<p class="about-description">
+			CAOS for Webfonts automagically saves the fonts you want to use inside your content-folder, generates a stylesheet for them and enqueues it in your theme's header.
+            This will decrease your pageload times, leverage your browser cache, minimize external requests and effectively bring you a perfect score on Pagespeed Insights and Pingdom.
+		</p>
+		<div class="welcome-panel-column-container">
+			<div class="welcome-panel-column">
+				<h3>Quickstart</h3>
+				<ul>
+					<li class="welcome-icon dashicons-before dashicons-editor-bold">Download fonts from <a href="https://fonts.google.com/" target="_blank">Google Fonts</a></li>
+					<li class="welcome-icon dashicons-before dashicons-admin-tools">Convert them using <a href="https://www.fontsquirrel.com/tools/webfont-generator" target="_blank">FontSquirrel's Webfont Generator</a></li>
+					<li class="welcome-icon dashicons-before dashicons-cloud">Upload/Add them using the 'Select Fonts' button</li>
+					<li class="welcome-icon dashicons-before dashicons-art">Click 'Generate Stylesheet'</li>
+					<li class="welcome-icon dashicons-before dashicons-media-text">Stylesheet is added to template and ready for use.</li>
+					<li class="welcome-icon dashicons-before dashicons-smiley">Done!</li>
+				</ul>
+                <p><a target="_blank" href="https://dev.daanvandenbergh.com/wordpress-plugins/host-google-fonts-locally/">Click here</a> for a more comprehensive guide.</p>
+			</div>
+			<div class="welcome-panel-column">
+				<h3>Get a Perfect Score on Pagespeed & Pingdom!</h3>
+                <p><strong>Leverage your browser cache</strong> and <strong>lower pageload times</strong> by hosting analytics.js locally with <a href="https://dev.daanvandenbergh.com/wordpress-plugins/optimize-analytics-wordpress/" target="_blank">CAOS for Analytics</a>.</p>
+                <p><a target="_blank" href="https://wordpress.org/plugins/host-analyticsjs-local">Download now</a></p>
+				<h3>Need help?</h3>
+				<p>First make sure you read the manual thoroughly.</p>
+                <p>Still running into issues? Visit the <a href="https://wordpress.org/plugins/host-webfonts-local/#description">FAQ</a> and <a href="https://wordpress.org/plugins/host-webfonts-local">Support Forum</a> to see if your question has already been answered.</p>
+                <p>If not, ask a question or <a href="https://dev.daanvandenbergh.com/contact" target="_blank">contact me</a>.</p>
+			</div>
+			<div class="welcome-panel-column welcome-panel-last">
+				<h3>Did I make you happy?</h3>
+				<p>I create simple, but useful solutions that easily optimize your website for Pagespeed Insights and Pingdom.</p>
+				<p>I believe these solutions should be free, but if you appreciate my work, let's have a beer together!</p>
+				<a target="_blank" class="button button-primary button-hero" href="https://dev.daanvandenbergh.com/donate/"><span class="dashicons-before dashicons-heart"> Sure. This round's on me!</span></a>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== CAOS for Webfonts - Host your Webfonts Locally! ===
+Contributors: DaanvandenBergh
+Donate link: https://dev.daanvandenbergh.com/donate/
+Tags: update, host, save, local, locally, google, fonts, webfonts, minimize, external, requests, leverage, browser, cache
+Requires at least: 4.5
+Tested up to: 4.9
+Stable tag: 1.56
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automagically saves the fonts you want to use inside your content-folder, generates a stylesheet for them and enqueues it in your theme's header.
+
+== Description ==
+
+Another cool plugin created by [Daan van den Bergh](http://dev.daanvandenbergh.com "Click here to visit my Wordpress Development Blog") that automagically saves the fonts you want to use inside your content-folder, generates a stylesheet for them and enqueues it in your theme's header.
+
+This will *decrease your pageload times*, *leverage browser cache*, *minimize DNS requests* and effectively bring you a perfect score on *Pagespeed Insights* and *Pingdom*.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/host-webforms-local` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+1. Use the Settings -> Optimize Webfonts screen to configure the plugin
+
+== Frequently Asked Questions ==
+
+= Can I buy you a beer? =
+
+Yes, please! [Click here to buy me a beer](http://dev.daanvandenbergh.com/donate/ "Let's do shots!")!
+
+== Screenshots ==
+
+N/A
+
+== Changelog ==
+
+= 1.0 =
+First release! No changes so far!
