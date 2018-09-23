011249a6b807d9ab7bedec15c4679520ace9b8c2
Dan0sz <info@daanvandenbergh.nl>
2018-09-23 20:53:28 +0200

    Fixed merge conflict

--- host-webfonts-local.php
+++ host-webfonts-local.php
@@ -3,11 +3,7 @@
  * Plugin Name: CAOS for Webfonts
  * Plugin URI: https://dev.daanvandenbergh.com/wordpress-plugins/host-google-fonts-locally
  * Description: Automagically save the fonts you want to use inside your content-folder, generate a stylesheet for them and enqueue it in your theme's header.
-<<<<<<< HEAD
  * Version: 1.2.6
-=======
- * Version: 1.1.0
->>>>>>> 7ecb0a932105ee49eefc6048af13862838134180
  * Author: Daan van den Bergh
  * Author URI: https://dev.daanvandenbergh.com
  * License: GPL2v2 or later
@@ -30,7 +26,7 @@ function hwlSetAllowedFiletypes($filetypes = array()) {
 	$filetypes['svg']   = 'image/svg+xml';
 	$filetypes['eot']   = 'application/vnd.ms-fontobject';
 
-    return $filetypes;
+	return $filetypes;
 }
 add_filter('upload_mimes', 'hwlSetAllowedFiletypes');
 
@@ -47,13 +43,13 @@ define('CAOS_WEBFONTS_UPLOAD_URL', content_url() . CAOS_WEBFONTS_CACHE_DIR);
  */
 function hwlCreateMenu()
 {
-    add_options_page(
-        'CAOS for Webfonts',
-        'Optimize Webfonts',
-        'manage_options',
-        'optimize-webfonts',
-        'hwlSettingsPage'
-    );
+	add_options_page(
+		'CAOS for Webfonts',
+		'Optimize Webfonts',
+		'manage_options',
+		'optimize-webfonts',
+		'hwlSettingsPage'
+	);
 }
 add_action('admin_menu', 'hwlCreateMenu');
 
@@ -62,277 +58,107 @@ add_action('admin_menu', 'hwlCreateMenu');
  */
 function hwlSettingsPage()
 {
-    if (!current_user_can('manage_options'))
-    {
-        wp_die(__("You're not cool enough to access this page."));
-    }
-    ?>
+	if (!current_user_can('manage_options'))
+	{
+		wp_die(__("You're not cool enough to access this page."));
+	}
+	?>
     <div class="wrap">
         <h1><?php _e('CAOS for Webfonts', 'host-webfonts-local'); ?></h1>
         <p>
-		    <?php _e('Developed by: ', 'host-webfonts-local'); ?>
+			<?php _e('Developed by: ', 'host-webfonts-local'); ?>
             <a title="Buy me a beer!" href="https://dev.daanvandenbergh.com/donate/">
                 Daan van den Bergh</a>.
         </p>
         <div id="hwl-admin-notices"></div>
-        <?php require_once('includes/welcome-panel.php'); ?>
+		<?php require_once('includes/welcome-panel.php'); ?>
         <form id="hwl-options-form" name="hwl-options-form">
-            <?php
-            settings_fields('host-webfonts-local-basic-settings'
-            );
-            do_settings_sections('host-webfonts-local-basic-settings'
-            );
+			<?php
+			settings_fields('host-webfonts-local-basic-settings'
+			);
+			do_settings_sections('host-webfonts-local-basic-settings'
+			);
 
-            /**
-             * Render the upload-functions.
-             */
-            hwlMediaUploadInit();
+			/**
+			 * Render the upload-functions.
+			 */
+			hwlMediaUploadInit();
 
-            do_action('hwl_after_form_settings');
-            ?>
+			do_action('hwl_after_form_settings');
+			?>
         </form>
     </div>
-    <?php
+	<?php
 }
 
 /**
  * Set custom upload-fields and render upload buttons.
  */
 function hwlMediaUploadInit() {
-    wp_enqueue_media();
-    ?>
+	wp_enqueue_media();
+	?>
     <table>
         <tbody>
-            <tr valign="top">
-                <td colspan="2">
-                    <input type="text" name="search-field"
-                           id="search-field" class="form-input-tip ui-autocomplete-input" placeholder="Search fonts..." />
-                </td>
-            </tr>
+        <tr valign="top">
+            <td colspan="2">
+                <input type="text" name="search-field"
+                       id="search-field" class="form-input-tip ui-autocomplete-input" placeholder="Search fonts..." />
+            </td>
+        </tr>
         </tbody>
-            <tr valign="top">
-                <th>
-                    font-family
-                </th>
-                <th>
-                    font-style
-                </th>
-                <th>
-                    remove
-                </th>
-            </tr>
+        <tr valign="top">
+            <th>
+                font-family
+            </th>
+            <th>
+                font-style
+            </th>
+            <th>
+                remove
+            </th>
+        </tr>
         <tbody id="hwl-results">
-            <tr class="loading" style="display: none;">
-                <td colspan="3" align="center">
-                    <span class="spinner"></span>
-                </td>
-            </tr>
-            <tr class="error" style="display: none;">
-                <td colspan="3" align="center">No fonts available.</td>
-            </tr>
+        <tr class="loading" style="display: none;">
+            <td colspan="3" align="center">
+                <span class="spinner"></span>
+            </td>
+        </tr>
+        <tr class="error" style="display: none;">
+            <td colspan="3" align="center">No fonts available.</td>
+        </tr>
         </tbody>
     </table>
 
     <table>
         <tbody>
-            <tr valign="bottom">
-                <td>
-                    <input type="button" onclick="hwlGenerateStylesheet()" name="generate-btn"
-                           id="generate-btn" class="button-primary" value="Generate Stylesheet" />
-                </td>
-            </tr>
+        <tr valign="bottom">
+            <td>
+                <input type="button" onclick="hwlGenerateStylesheet()" name="generate-btn"
+                       id="generate-btn" class="button-primary" value="Generate Stylesheet" />
+            </td>
+        </tr>
         </tbody>
     </table>
     <script type="text/javascript">
-<<<<<<< HEAD
-=======
-        var media_uploader = null;
-
-        /**
-         * Get the Media Uploader and prepare the uploaded fonts for generating the style sheet.
-         */
-        function hwlFontUploader()
-        {
-            media_uploader = wp.media({
-                    frame: "post",
-                    state: "insert",
-                    multiple: true
-                }).open();
-
-            hwlSetUploadDir();
-
-            media_uploader.on(
-                "close",
-                function() {
-                    hwlResetUploadDir();
-                }
-            ).on(
-                "insert",
-                function() {
-                    hwlGenerateOutput();
-                }
-            );
-        }
-
-        /**
-         * Call the media-upload script or logs an error to the console.
-         */
-        function hwlSetUploadDir()
-        {
-            jQuery.ajax({
-                type: 'POST',
-                url: ajaxurl,
-                data: {
-                    action: 'hwlAjaxSetUploadDir'
-                },
-                error: function(response) {
-                    console.log(response);
-                }
-            });
-        }
-
-        /**
-         *  AJAX call to reset upload directory.
-         */
-        function hwlResetUploadDir()
-        {
-            jQuery.ajax({
-                type: 'POST',
-                url: ajaxurl,
-                data: {
-                    action: 'hwlAjaxResetUploadDir'
-                },
-                error: function(response) {
-                    console.log(response);
-                }
-            });
-        }
-
-        /**
-         * Generate the output after upload/insert
-         */
-        function hwlGenerateOutput()
-        {
-            var length = media_uploader.state().get("selection").length;
-            var fonts = media_uploader.state().get("selection").models;
-            for (var iii = 0; iii < length; iii++) {
-                var font_url = fonts[iii].changed.url;
-                var font_name = fonts[iii].changed.title;
-                var font_type = fonts[iii].changed.subtype;
-                var uploadedFont = `<tr valign="top">
-                                            <td>
-                                                <input type="text" name="hwl_uploaded_font][${font_name}]"
-                                                       id="hwl_uploaded_font][${font_name}]"
-                                                       value="${font_name}" readonly />
-                                            </td>
-                                            <td>
-                                                <input type="text" name="hwl_uploaded_font][${font_name}][font_family]"
-                                                       id="hwl_uploaded_font][${font_name}][font_family]"
-                                                       value="" />
-                                            </td>
-                                            <td>
-                                                <input type="text" name="hwl_uploaded_font][${font_name}][font_weight]"
-                                                       id="hwl_uploaded_font][${font_name}][font_weight]"
-                                                       value="" />
-                                            </td>
-                                            <td>
-                                                <input type="text" name="hwl_uploaded_font][${font_name}][${font_type}]"
-                                                       id="hwl_uploaded_font][${font_name}][${font_type}]"
-                                                       value="${font_type}" readonly />
-                                            </td>
-                                            <td>
-                                                <input type="text" name="hwl_uploaded_font][${font_name}][${font_type}][url]"
-                                                       id="hwl_uploaded_font][${font_name}][${font_type}][url]"
-                                                       value="${font_url}" readonly />
-                                            </td>
-                                        </tr>`;
-                jQuery('#hwl_uploaded_fonts').append(uploadedFont);
-            }
-        }
-
-        /**
-         * Call the generate-stylesheet script and reset the upload dir to the default setting.
-         */
-        function hwlGenerateStylesheet() {
-            var hwlData = hwlSerializeArray($('#hwl-options-form'));
-
-            jQuery.ajax({
-                type: 'POST',
-                url: ajaxurl,
-                data: {
-                    action: 'hwlAjaxGenerateStyles',
-                    uploaded_fonts: hwlData
-                },
-                success: function(response) {
-                    jQuery('#hwl-admin-notices').append(
-                        `<div class="updated settings-error notice is-dismissible">
-                            <p>${response}</p>
-                        </div>`
-                    );
-                    jQuery('#hwl_uploaded_fonts').each(function() {
-                        jQuery(this).fadeOut(700, function() {
-                            jQuery(this).remove();
-                        })
-                    });
-                },
-                error: function(response) {
-                    jQuery('#hwl-admin-notices').append(
-                        `<div class="notice notice-error is-dismissible">
-                            <p><?php _e( 'The stylesheet could not be created:', 'host-webfonts-local' ); ?> ${response}</p>"
-                        </div>`
-                    )
-                }
-            });
-        }
-
-        /**
-         * Serialize form data to a multi-dimensional array.
-         */
-        function hwlSerializeArray(data) {
-            var result = [];
-            data.each(function() {
-                var fields = {};
-                $.each($(this).serializeArray(), function() {
-                    fields[this.name] = this.value;
-                });
-                result.push(fields);
-            });
-
-            return result;
-        }
->>>>>>> 7ecb0a932105ee49eefc6048af13862838134180
     </script>
-<?php
+	<?php
 }
 
-<<<<<<< HEAD
 function hwlAjaxSearchGoogleFonts() {
-    try {
-        $request = curl_init();
+	try {
+		$request = curl_init();
 
-        curl_setopt($request, CURLOPT_URL, 'https://google-webfonts-helper.herokuapp.com/api/fonts/' . $_POST['search_query']);
-        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($request, CURLOPT_URL, 'https://google-webfonts-helper.herokuapp.com/api/fonts/' . $_POST['search_query']);
+		curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
 
-	    $result  = curl_exec($request);
+		$result  = curl_exec($request);
 
-	    curl_close($request);
-	    wp_die($result);
-=======
-/**
- * Before each upload we temporarily set our custom upload-directory.
- */
-function hwlAjaxSetUploadDir() {
-    try {
-	    update_option('upload_path',WP_CONTENT_DIR . '/local-fonts');
-	    update_option('upload_url_path',content_url() . '/local-fonts');
-	    update_option('uploads_use_yearmonth_folders', false);
-	    wp_die();
->>>>>>> 7ecb0a932105ee49eefc6048af13862838134180
-    } catch (\Exception $e) {
-        wp_die($e);
-    }
+		curl_close($request);
+		wp_die($result);
+	} catch (\Exception $e) {
+		wp_die($e);
+	}
 }
-<<<<<<< HEAD
 add_action('wp_ajax_hwlAjaxSearchGoogleFonts', 'hwlAjaxSearchGoogleFonts');
 
 /**
@@ -346,31 +172,12 @@ function hwlCreateCacheDir()
 	}
 }
 register_activation_hook(__FILE__, 'hwlCreateCacheDir' );
-=======
-add_action('wp_ajax_hwlAjaxSetUploadDir', 'hwlAjaxSetUploadDir');
-
-
-/**
- * After we're done uploading we need to reset the upload-directory.
- */
-function hwlAjaxResetUploadDir() {
-    try {
-	    update_option('upload_path',null);
-	    update_option('upload_url_path',null);
-	    update_option('uploads_use_yearmonth_folders', true);
-	    wp_die();
-    } catch (\Exception $e) {
-        wp_die($e);
-    }
-}
-add_action('wp_ajax_hwlAjaxResetUploadDir', 'hwlAjaxResetUploadDir');
->>>>>>> 7ecb0a932105ee49eefc6048af13862838134180
 
 /**
  * The function for generating the stylesheet and resetting the upload dir to the default.
  */
 function hwlAjaxGenerateStyles() {
-    require_once('includes/generate-stylesheet.php');
+	require_once('includes/generate-stylesheet.php');
 }
 add_action('wp_ajax_hwlAjaxGenerateStyles', 'hwlAjaxGenerateStyles');
 
@@ -379,17 +186,17 @@ add_action('wp_ajax_hwlAjaxGenerateStyles', 'hwlAjaxGenerateStyles');
  */
 function hwlEnqueueStylesheet()
 {
-    $stylesheet = CAOS_WEBFONTS_UPLOAD_DIR . '/'. CAOS_WEBFONTS_FILENAME;
-    if (file_exists($stylesheet)) {
-	    wp_register_style('hwl-style', CAOS_WEBFONTS_UPLOAD_URL . '/' . CAOS_WEBFONTS_FILENAME);
-	    wp_enqueue_style('hwl-style');
-    }
+	$stylesheet = CAOS_WEBFONTS_UPLOAD_DIR . '/'. CAOS_WEBFONTS_FILENAME;
+	if (file_exists($stylesheet)) {
+		wp_register_style('hwl-style', CAOS_WEBFONTS_UPLOAD_URL . '/' . CAOS_WEBFONTS_FILENAME);
+		wp_enqueue_style('hwl-style');
+	}
 }
 add_action('wp_enqueue_scripts', 'hwlEnqueueStylesheet' );
 
 function hwlEnqueueAdminJs()
 {
-    wp_enqueue_script('hwl-admin-js', plugin_dir_url(__FILE__) . 'js/hwl-admin.js', array('jquery'), null, true);
-    wp_enqueue_style('hwl-admin.css', plugin_dir_url(__FILE__) . 'css/hwl-admin.css');
+	wp_enqueue_script('hwl-admin-js', plugin_dir_url(__FILE__) . 'js/hwl-admin.js', array('jquery'), null, true);
+	wp_enqueue_style('hwl-admin.css', plugin_dir_url(__FILE__) . 'css/hwl-admin.css');
 }
 add_action('admin_enqueue_scripts', 'hwlEnqueueAdminJs');
