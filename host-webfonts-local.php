1c7afd6f32da9f7f5dd33767f9ce3b332935d751
Dan0sz <info@daanvandenbergh.nl>
2018-09-29 17:11:16 +0200

    Fixed merge error

--- host-webfonts-local.php
+++ host-webfonts-local.php
@@ -3,7 +3,7 @@
  * Plugin Name: CAOS for Webfonts
  * Plugin URI: https://dev.daanvandenbergh.com/wordpress-plugins/host-google-fonts-locally
  * Description: Automagically save the fonts you want to use inside your content-folder, generate a stylesheet for them and enqueue it in your theme's header.
- * Version: 1.2.7
+ * Version: 1.2.6
  * Author: Daan van den Bergh
  * Author URI: https://dev.daanvandenbergh.com
  * License: GPL2v2 or later
