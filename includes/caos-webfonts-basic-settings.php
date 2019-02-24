a1754f9a5a8c49fa0b563b1ef530777af7b13cca
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2019-02-24 23:05:32 +0100

    Added experimental option to enable preload resource hint.

--- includes/caos-webfonts-basic-settings.php
+++ includes/caos-webfonts-basic-settings.php
@@ -13,7 +13,9 @@ if (!defined( 'ABSPATH')) exit;
 	<h3><?php _e('Basic Settings'); ?></h3>
 	<table class="form-table">
 		<tr valign="top">
-			<th scope="row"><?php _e('Save webfonts to...', 'host-webfonts-local'); ?></th>
+			<th scope="row">
+                <label><?php _e('Save webfonts to...', 'host-webfonts-local'); ?></label>
+            </th>
 			<td>
 				<input class="caos_webfonts_cache_dir" type="text" name="caos_webfonts_cache_dir" placeholder="e.g. /cache/caos-webfonts" value="<?php echo CAOS_WEBFONTS_CACHE_DIR; ?>" />
 				<p class="description">
@@ -25,17 +27,28 @@ if (!defined( 'ABSPATH')) exit;
             <th scope="row"><?php _e('Font-display option', 'host-webfonts-local'); ?></th>
             <td>
                 <?php $fontDisplay = hwlFontDisplayOptions(); ?>
-                    <label>
-                        <select name="caos_webfonts_display_option">
-	                        <?php foreach ($fontDisplay as $label => $value): ?>
-                            <option value="<?php echo $value; ?>" <?php echo $value == CAOS_WEBFONTS_DISPLAY_OPTION ? 'selected' : ''; ?>><?php _e($label, 'host-webfonts-local'); ?></option>
-	                        <?php endforeach; ?>
-                        </select>
-                    </label>
+                    <select name="caos_webfonts_display_option">
+                        <?php foreach ($fontDisplay as $label => $value): ?>
+                        <option value="<?php echo $value; ?>" <?php echo $value == CAOS_WEBFONTS_DISPLAY_OPTION ? 'selected' : ''; ?>><?php _e($label, 'host-webfonts-local'); ?></option>
+                        <?php endforeach; ?>
+                    </select>
                     <br/>
                 <p class="description">
 		            <?php _e('Select which font-display strategy to use. Defaults to \'Auto\'.', 'host-webfonts-local'); ?>
                     <a target="_blank" href="https://developers.google.com/web/updates/2016/02/font-display"><?php _e('Read more', 'host-webfonts-local'); ?></a>
+                </p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">
+                <label>
+                    <?php _e('Enable preload for stylesheet (experimental)', 'host-webfonts-local'); ?>
+                </label>
+            </th>
+            <td>
+                <input class="caos_webfonts_preload" type="checkbox" name="caos_webfonts_preload" <?php echo CAOS_WEBFONTS_PRELOAD == 'on' ? 'checked = "checked"' : ''; ?> />
+                <p class="description">
+                    <?php _e('If you\'re using a plugin (such as Autoptimize) to load stylesheets in the footer, enable this to preload the fonts.', 'host-webfonts-local'); ?> <a target="_blank" href="https://developers.google.com/web/fundamentals/performance/resource-prioritization#preload"><?php _e('Read more', 'host-webfonts-local'); ?></a>
                 </p>
             </td>
         </tr>
