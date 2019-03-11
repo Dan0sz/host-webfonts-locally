87af51e94ff512fcc9caadc24ca041417cb41658
DaanvandenBergh <DaanvandenBergh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-03-11 21:10:33 +0000

    Update to v1.7.1
    git-svn-id: http://plugins.svn.wordpress.org/host-webfonts-local/trunk@2048638 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- host-webfonts-local.php
+++ host-webfonts-local.php
@@ -3,7 +3,7 @@
  * Plugin Name: CAOS for Webfonts
  * Plugin URI: https://daan.dev/wordpress-plugins/host-google-fonts-locally
  * Description: Automagically save the fonts you want to use inside your content-folder, generate a stylesheet for them and enqueue it in your theme's header.
- * Version: 1.7.0
+ * Version: 1.7.1
  * Author: Daan van den Bergh
  * Author URI: https://daan.dev
  * License: GPL2v2 or later
