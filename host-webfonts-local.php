1952a28fd794c77bcd300e56447c7e930fcb70ea
DaanvandenBergh <DaanvandenBergh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-09-22 13:34:31 +0000

    v1.9.5
    git-svn-id: http://plugins.svn.wordpress.org/host-webfonts-local/trunk@2160958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- host-webfonts-local.php
+++ host-webfonts-local.php
@@ -4,7 +4,7 @@
  * Plugin Name: OMGF
  * Plugin URI: https://daan.dev/wordpress-plugins/host-google-fonts-locally
  * Description: Automagically save the fonts you want to use inside your content-folder, generate a stylesheet for them and enqueue it in your theme's header.
- * Version: 1.9.4
+ * Version: 1.9.5
  * Author: Daan van den Bergh
  * Author URI: https://daan.dev
  * License: GPL2v2 or later
@@ -460,7 +460,7 @@ add_action('wp_ajax_hwlAjaxDownloadFonts', 'hwlAjaxDownloadFonts');
  */
 function hwlEnqueueStylesheet()
 {
-    wp_enqueue_style('hwl-style', CAOS_WEBFONTS_UPLOAD_URL . '/' . CAOS_WEBFONTS_FILENAME, array(), (CAOS_WEBFONTS_REMOVE_VERSION) ? null : CAOS_WEBFONTS_STATIC_VERSION);
+    wp_enqueue_style('omgf-fonts', CAOS_WEBFONTS_UPLOAD_URL . '/' . CAOS_WEBFONTS_FILENAME, array(), (CAOS_WEBFONTS_REMOVE_VERSION) ? null : CAOS_WEBFONTS_STATIC_VERSION);
 }
 add_action('wp_enqueue_scripts', 'hwlEnqueueStylesheet');
 
@@ -511,21 +511,32 @@ add_action('init', 'hwlIsPreloadEnabled');
 
 /**
  * Automatically dequeues any stylesheets loaded from fonts.gstatic.com or
- * fonts.googleapis.com.
+ * fonts.googleapis.com. Also checks for stylesheets dependant on Google Fonts and
+ * re-enqueues and registers them.
  */
 function hwlRemoveGoogleFonts()
 {
     global $wp_styles;
 
     $registered = $wp_styles->registered;
-    $fonts      = array_filter($registered, function ($contents, $handle) {
+
+    $fonts      = array_filter($registered, function ($contents) {
         return strpos($contents->src, 'fonts.googleapis.com') !== false
                || strpos($contents->src, 'fonts.gstatic.com') !== false;
-    }, ARRAY_FILTER_USE_BOTH);
+    });
+
+    $dependencies = array_filter($registered, function ($contents) use ($fonts) {
+        return !empty(array_intersect(array_keys($fonts), $contents->deps));
+    });
 
     foreach ($fonts as $font) {
         wp_deregister_style($font->handle);
         wp_dequeue_style($font->handle);
+    }
+
+    foreach ($dependencies as $dependency) {
+        wp_register_style('omgf-dep-' . $dependency->handle, $dependency->src);
+        wp_enqueue_style('omgf-dep-' . $dependency->handle, $dependency->src);
     }
 }
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://daan.dev/donate/
 Tags: google, fonts, host, save, local, locally, webfonts, update, minimize, external, requests, leverage, browser, cache
 Requires at least: 4.6
 Tested up to: 5.2
-Stable tag: 1.9.4
+Stable tag: 1.9.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -88,6 +88,9 @@ Yes, please! [Click here to buy me a beer](https://daan.dev/donate/ "Let's do sh
 N/A
 
 == Changelog ==
+
+= 1.9.5 =
+'Remove Google Fonts'-options now re-enqueues styles that were dependent on removed Fonts.
 
 = 1.9.4 =
 Small improvement to 'Remove Google Fonts'-option.
