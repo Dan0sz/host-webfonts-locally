ffaaecc77ccf2dd99ab18ed3d7352b1ccbd78c95
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2020-09-25 09:46:24 +0200

    Re-factored Pro Promo Options.

--- includes/admin/settings/class-extensions.php
+++ includes/admin/settings/class-extensions.php
@@ -32,10 +32,10 @@ class OMGF_Admin_Settings_Extensions extends OMGF_Admin_Settings_Builder
         add_filter('omgf_extensions_settings_content', [$this, 'do_before'], 20);
 
         // Settings
-        add_filter('omgf_extensions_settings_content', [$this, 'do_promo_remove_stylesheets'], 30);
-        add_filter('omgf_extensions_settings_content', [$this, 'do_promo_remove_inline_styles'], 40);
-        add_filter('omgf_extensions_settings_content', [$this, 'do_promo_remove_webfont_loader'], 50);
-        add_filter('omgf_extensions_settings_content', [$this, 'do_promo_remove_resource_hints'], 60);
+        add_filter( 'omgf_extensions_settings_content', [ $this, 'do_promo_process_stylesheets' ], 30);
+        add_filter( 'omgf_extensions_settings_content', [ $this, 'do_promo_process_inline_styles' ], 40);
+        add_filter( 'omgf_extensions_settings_content', [ $this, 'do_promo_process_webfont_loader' ], 50);
+        add_filter( 'omgf_extensions_settings_content', [ $this, 'do_promo_process_resource_hints' ], 60);
 
         // Close
         add_filter('omgf_extensions_settings_content', [$this, 'do_after'], 100);
@@ -60,12 +60,12 @@ class OMGF_Admin_Settings_Extensions extends OMGF_Admin_Settings_Builder
     /**
      * Add (overridable) promo options for Remove Stylesheets Pro.
      */
-    public function do_promo_remove_stylesheets()
+    public function do_promo_process_stylesheets()
     {
         $this->do_checkbox(
-                __('Remove Stylesheets (Pro)', $this->plugin_text_domain),
+                __('Process Stylesheets (Pro)', $this->plugin_text_domain),
                 'omgf_pro_remove_stylesheets',
-                defined('OMGF_PRO_REMOVE_STYLESHEETS') ? OMGF_PRO_PROCESS_STYLESHEETS : false,
+                defined('OMGF_PRO_PROCESS_STYLESHEETS') ? OMGF_PRO_PROCESS_STYLESHEETS : false,
                 sprintf(__('Remove all stylesheets loaded from <code>fonts.googleapis.com</code> or <code>fonts.gstatic.com</code>. <a href="%s" target="_blank">Purchase OMGF Pro</a> to enable this option.', $this->plugin_text_domain), OMGF_Admin_Settings_Builder::FFWP_WORDPRESS_PLUGINS_OMGF_PRO),
                 false,
                 true
@@ -75,12 +75,12 @@ class OMGF_Admin_Settings_Extensions extends OMGF_Admin_Settings_Builder
     /**
      *
      */
-    public function do_promo_remove_inline_styles()
+    public function do_promo_process_inline_styles()
     {
         $this->do_checkbox(
-            __('Remove Inline Styles (Pro)', $this->plugin_text_domain),
+            __('Process Inline Styles (Pro)', $this->plugin_text_domain),
             'omgf_pro_remove_inline_styles',
-            defined('OMGF_PRO_REMOVE_INLINE_STYLES') ? OMGF_PRO_PROCESS_INLINE_STYLES : false,
+            defined('OMGF_PRO_PROCESS_INLINE_STYLES') ? OMGF_PRO_PROCESS_INLINE_STYLES : false,
             sprintf(__('Remove all <code>@font-face</code> and <code>@import</code> rules loading Google Fonts. <a href="%s" target="_blank">Purchase OMGF Pro</a> to enable this option.', $this->plugin_text_domain), OMGF_Admin_Settings_Builder::FFWP_WORDPRESS_PLUGINS_OMGF_PRO),
             false,
             true
@@ -90,12 +90,12 @@ class OMGF_Admin_Settings_Extensions extends OMGF_Admin_Settings_Builder
     /**
      *
      */
-    public function do_promo_remove_webfont_loader()
+    public function do_promo_process_webfont_loader()
     {
         $this->do_checkbox(
-            __('Remove WebFont Loader (Pro)', $this->plugin_text_domain),
+            __('Process WebFont Loader (Pro)', $this->plugin_text_domain),
             'omgf_pro_remove_webfont_loader',
-            defined('OMGF_PRO_REMOVE_WEBFONT_LOADER') ? OMGF_PRO_PROCESS_WEBFONT_LOADER : false,
+            defined('OMGF_PRO_PROCESS_WEBFONT_LOADER') ? OMGF_PRO_PROCESS_WEBFONT_LOADER : false,
             sprintf(__('Remove any WebFont Loader (<code>webfont.js</code>) libraries and the corresponding configuration defining which Google Fonts to load (WebFont Config). <a href="%s" target="_blank">Purchase OMGF Pro</a> to enable this option.', $this->plugin_text_domain), OMGF_Admin_Settings_Builder::FFWP_WORDPRESS_PLUGINS_OMGF_PRO),
             false,
             true
@@ -105,12 +105,12 @@ class OMGF_Admin_Settings_Extensions extends OMGF_Admin_Settings_Builder
     /**
      *
      */
-    public function do_promo_remove_resource_hints()
+    public function do_promo_process_resource_hints()
     {
         $this->do_checkbox(
-            __('Remove Resource Hints (Pro)', $this->plugin_text_domain),
+            __('Process Resource Hints (Pro)', $this->plugin_text_domain),
             'omgf_pro_remove_resource_hints',
-            defined('OMGF_PRO_REMOVE_RESOURCE_HINTS') ? OMGF_PRO_PROCESS_RESOURCE_HINTS : false,
+            defined('OMGF_PRO_PROCESS_RESOURCE_HINTS') ? OMGF_PRO_PROCESS_RESOURCE_HINTS : false,
             sprintf(__('Remove all <code>link</code> elements with a <code>rel</code> attribute value of <code>dns-prefetch</code>, <code>preload</code> or <code>preconnect</code>. <a href="%s" target="_blank">Purchase OMGF Pro</a> to enable this option.', $this->plugin_text_domain), OMGF_Admin_Settings_Builder::FFWP_WORDPRESS_PLUGINS_OMGF_PRO),
             false,
             true
