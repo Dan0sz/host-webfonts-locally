7b843440cb30ffc406792d620d91091e0312bdb0
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2020-09-29 21:33:17 +0200

    Removed obsolete constant.

--- includes/class-omgf.php
+++ includes/class-omgf.php
@@ -47,7 +47,6 @@ class OMGF
     public function define_constants()
     {
         define('OMGF_SITE_URL', 'https://daan.dev');
-        define('OMGF_HELPER_URL', 'https://google-webfonts-helper.herokuapp.com/api/fonts/');
         define('OMGF_FONT_PROCESSING', esc_attr(get_option(OMGF_Admin_Settings::OMGF_BASIC_SETTING_FONT_PROCESSING, 'replace')));
 	    define('OMGF_DISPLAY_OPTION', esc_attr(get_option( OMGF_Admin_Settings::OMGF_BASIC_SETTING_DISPLAY_OPTION, 'swap')) ?: 'swap');
 	    define('OMGF_OPTIMIZE_EDIT_ROLES', esc_attr(get_option( OMGF_Admin_Settings::OMGF_BASIC_SETTING_OPTIMIZE_EDIT_ROLES, 'on')));
