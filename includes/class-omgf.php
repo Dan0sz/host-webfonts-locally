2ba79f7cb248a970b8bed9123f0e08c6040e507e
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2021-09-29 08:03:31 +0000

    Fix: use array_merge to add new stylesheet to cache layer.

--- includes/class-omgf.php
+++ includes/class-omgf.php
@@ -118,8 +118,8 @@ class OMGF
 		/**
 		 * If $maybe_add doesn't exist in the cache layer yet, add it.
 		 */
-		if (!isset($optimized_fonts[key($maybe_add)])) {
-			$optimized_fonts = $optimized_fonts + $maybe_add;
+		if (!empty($maybe_add) && !isset($optimized_fonts[key($maybe_add)])) {
+			$optimized_fonts = array_merge($optimized_fonts, $maybe_add);
 		}
 
 		return $optimized_fonts;
