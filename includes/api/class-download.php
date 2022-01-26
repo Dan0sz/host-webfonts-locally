61fcea9ea30a00d217ad7af3d870f288a5c1b8ab
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2022-01-26 12:18:49 +0000

    Hotfix: API doesn't have to die post V4, since it's no longer a public API.

--- includes/api/class-download.php
+++ includes/api/class-download.php
@@ -101,7 +101,7 @@ class OMGF_API_Download extends WP_REST_Controller
         $original_handle = sanitize_title_with_dashes($request->get_param('original_handle'));
 
         if (!$this->handle || !$original_handle) {
-            wp_die(__('Handle not provided.', $this->plugin_text_domain), 406);
+            OMGF_Admin_Notice::set_notice(sprintf(__('OMGF API couldn\'t find required handle parameter. Values sent were %s and %s.', $this->plugin_text_domain), $original_handle, $this->handle));
         }
 
         $this->path       = WP_CONTENT_DIR . OMGF_CACHE_PATH . '/' . $this->handle;
