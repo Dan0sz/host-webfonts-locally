b35a3a5f6b8b5b8de350bc5bada054990fe5d10d
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2022-03-03 23:35:29 +0000

    Fixed: configure link wouldn't work for modified stylesheets.

--- includes/frontend/class-process.php
+++ includes/frontend/class-process.php
@@ -258,6 +258,8 @@ class OMGF_Frontend_Process
 	{
 		$google_fonts = [];
 
+		OMGF::debug(sprintf(__('Building fonts set for handle %s, using $s', 'host-webfonts-local'), $handle, print_r($links, true)));
+
 		foreach ($links as $key => $link) {
 			preg_match('/id=[\'"](?P<id>.*?)[\'"]/', $link, $id);
 
@@ -272,6 +274,8 @@ class OMGF_Frontend_Process
 			$google_fonts[$key]['id']   = $id;
 			$google_fonts[$key]['href'] = $href['href'];
 		}
+
+		OMGF::debug(sprintf(__('Built set: %s', 'host-webfonts-local'), print_r($google_fonts, true)));
 
 		return $google_fonts;
 	}
@@ -293,6 +297,8 @@ class OMGF_Frontend_Process
 	{
 		$search  = [];
 		$replace = [];
+
+		OMGF::debug(__('Building Search/Replace set...', 'host-webfonts-local'));
 
 		foreach ($google_fonts as $key => $stack) {
 			$handle = $stack['id'];
