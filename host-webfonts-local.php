e947257150418c457248fd17e96d389ec27eea9b
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2022-05-28 09:02:31 +0000

    Fixed: upped static version.

--- host-webfonts-local.php
+++ host-webfonts-local.php
@@ -19,7 +19,7 @@ defined('ABSPATH') || exit;
 define('OMGF_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('OMGF_PLUGIN_FILE', __FILE__);
 define('OMGF_PLUGIN_BASENAME', plugin_basename(OMGF_PLUGIN_FILE));
-define('OMGF_STATIC_VERSION', '5.1.1');
+define('OMGF_STATIC_VERSION', '5.1.4');
 define('OMGF_DB_VERSION', '5.0.0');
 
 /**
