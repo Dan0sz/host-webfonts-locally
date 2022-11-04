8d7cb23995413c1cdcc96c9db1f7ffa1a1fc11f0
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2022-11-04 15:10:43 +0000

    Improved: remove invalid characters when decode URL.

--- includes/class-omgf.php
+++ includes/class-omgf.php
@@ -200,6 +200,12 @@ class OMGF
 	/**
 	 * @since v5.3.3 Decode HTML entities to prevent URL decoding issues on some systems.
 	 * 
+	 * @since v5.4.3 With encoded URLs the Google Fonts API is much more lenient when it comes to invalid requests, 
+	 * 				 but we need the URL to be decoded in order to properly parsed (parse_str() and parse_url()), etc.
+	 * 				 So, as of now, we're trimming invalid characters from the end of the URL. The list will expand
+	 * 				 as I run into to them. I'm not going to make any assumptions on what theme/plugin developers 
+	 * 				 might be doing wrong.
+	 * 
 	 * @filter omgf_optimize_url
 	 * 
 	 * @param mixed $url 
@@ -208,7 +214,7 @@ class OMGF
 	 */
 	public function decode_url($url)
 	{
-		return html_entity_decode($url);
+		return rtrim(html_entity_decode($url), ',');
 	}
 
 	/**
