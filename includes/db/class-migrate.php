9999de9fd8acf1aaad0941df3a3f19679276ef67
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-04-01 19:25:56 +0000

    Added: DB migration script

--- includes/db/class-migrate.php
+++ includes/db/class-migrate.php
@@ -1,5 +1,5 @@
 <?php
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 /**
  * @package   OMGF Pro
@@ -7,38 +7,40 @@ defined('ABSPATH') || exit;
  *            https://daan.dev
  * @copyright © 2022 Daan van den Bergh. All Rights Reserved.
  */
-class OMGF_DB_Migrate
-{
-    /** @var string */
-    private $current_version = '';
+class OMGF_DB_Migrate {
 
-    /**
-     * DB Migration constructor.
-     */
-    public function __construct()
-    {
-        /**
-         * Can be used to block migration scripts that shouldn't be run on a fresh install.
-         */
-        $this->current_version = get_option(OMGF_Admin_Settings::OMGF_CURRENT_DB_VERSION);
+	/** @var string */
+	private $current_version = '';
 
-        if ($this->should_run_migration('5.3.3')) {
-            new OMGF_DB_Migrate_V533();
-        }
+	/**
+	 * DB Migration constructor.
+	 */
+	public function __construct() {
+		/**
+		 * Can be used to block migration scripts that shouldn't be run on a fresh install.
+		 */
+		$this->current_version = get_option( OMGF_Admin_Settings::OMGF_CURRENT_DB_VERSION );
 
-        if ($this->should_run_migration('5.3.4')) {
-            new OMGF_DB_Migrate_V534();
-        }
-    }
+		if ( $this->should_run_migration( '5.3.3' ) ) {
+			new OMGF_DB_Migrate_V533();
+		}
 
-    /**
-     * Checks whether migration script has been run.
-     * 
-     * @param mixed $version 
-     * @return bool 
-     */
-    private function should_run_migration($version)
-    {
-        return version_compare($this->current_version, $version) < 0;
-    }
+		if ( $this->should_run_migration( '5.3.4' ) ) {
+			new OMGF_DB_Migrate_V534();
+		}
+
+		if ( $this->should_run_migration( '5.5.7' ) ) {
+			new OMGF_DB_Migrate_V557();
+		}
+	}
+
+	/**
+	 * Checks whether migration script has been run.
+	 * 
+	 * @param mixed $version 
+	 * @return bool 
+	 */
+	private function should_run_migration( $version ) {
+		return version_compare( $this->current_version, $version ) < 0;
+	}
 }
