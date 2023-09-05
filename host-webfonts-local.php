0f062012758f6819e6e9b66121a6d5663b36a351
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-09-05 20:23:38 +0000

    v5.7.1

--- host-webfonts-local.php
+++ host-webfonts-local.php
@@ -3,7 +3,7 @@
  * Plugin Name: OMGF
  * Plugin URI: https://daan.dev/wordpress/omgf/
  * Description: Increase GDPR/DSGVO compliance, reduce DNS requests and leverage browser cache by automatically downloading Google Fonts to your server.
- * Version: 5.7.0
+ * Version: 5.7.1
  * Author: Daan from Daan.dev
  * Author URI: https://daan.dev
  * License: GPL2v2 or later
