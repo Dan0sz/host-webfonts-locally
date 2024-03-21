cfe5e7b56393d581103fc2657b74939a7877a002
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-03-21 12:51:21 +0100

    Added: AdminTest

---
+++ tests/integration/AdminTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @package OMGF integration tests - Admin
+ */
+
+namespace OMGF\Tests\Integration;
+
+use OMGF\Admin;
+use OMGF\Admin\Settings;
+use OMGF\Helper as OMGF;
+use OMGF\Tests\TestCase;
+
+class AdminTest extends TestCase {
+	/**
+	 * @see Admin::do_optimize_settings()
+	 * @return void
+	 */
+	public function testDoOptimizeSettings() {
+		new Admin();
+
+		$this->expectOutputContains( 'Optimize Local Google Fonts' );
+
+		do_action( 'omgf_optimize_settings_content' );
+	}
+
+	/**
+	 * @see Admin::do_detection_settings()
+	 * @return void
+	 */
+	public function testDoDetectionSettings() {
+		new Admin();
+
+		$this->expectOutputContains( 'Google Fonts Detection Settings' );
+
+		do_action( 'omgf_detection_settings_content' );
+	}
+
+	/**
+	 * @see Admin::do_advanced_settings()
+	 * @return void
+	 */
+	public function testDoAdvancedSettings() {
+		new Admin();
+
+		$this->expectOutputContains( 'Advanced Settings' );
+
+		do_action( 'omgf_advanced_settings_content' );
+	}
+
+	/**
+	 * @see Admin::do_help()
+	 * @return void
+	 */
+	public function testDoHelp() {
+		new Admin();
+
+		$this->expectOutputContains( 'Help &amp; Documentation' );
+
+		do_action( 'omgf_help_content' );
+	}
+
+	/**
+	 * @see Admin::maybe_show_stale_cache_notice()
+	 * @return void
+	 */
+	public function testShowStaleCacheNotice() {
+		$class = new Admin();
+
+		$_GET[ 'page' ] = Settings::OMGF_ADMIN_PAGE;
+		$_GET[ 'tab' ]  = 'test';
+
+		$class->maybe_show_stale_cache_notice( [ 'subsets' => [ 'latin-ext' ] ], [ 'subsets' => [ 'latin' ] ] );
+
+		$this->assertTrue( OMGF::get_option( Settings::OMGF_CACHE_IS_STALE ) );
+	}
+}
