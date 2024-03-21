fa2c7cd19f58f1c62c1d17e440825748fb7f93ed
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-03-21 13:00:48 +0100

    Added: PluginTest

---
+++ tests/integration/PluginTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * OMGF: Testing Plugin Main initialization.
+ */
+
+namespace OMGF\Tests\Integration;
+
+use OMGF\Plugin;
+use OMGF\Tests\TestCase;
+
+class PluginTest extends TestCase {
+	/**
+	 * This test isn't very meaningful, besides the fact that it executes the Plugin's init code.
+	 * @return void
+	 */
+	public function testInit() {
+		require_once( OMGF_TESTS_ROOT . '../host-webfonts-local.php' );
+
+		new Plugin();
+
+		$this->assertTrue( class_exists( '\OMGF\Filters' ) );
+	}
+
+	/**
+	 * This test isn't very meaningful, besides the fact that it executes the Plugin's Admin init code.
+	 * @return void
+	 */
+	public function testInitAdmin() {
+		if ( ! defined( 'WP_ADMIN' ) ) {
+			define( 'WP_ADMIN', true );
+		}
+
+		new Plugin();
+
+		$this->assertTrue( class_exists( '\OMGF\Admin\Actions' ) );
+	}
+
+	/**
+	 * @see  Plugin::do_migrate_db()
+	 * @return void
+	 * @todo Make this test meaningful.
+	 */
+	public function testMigrateDb() {
+		new Plugin();
+
+		do_action( 'plugins_loaded' );
+
+		$this->assertTrue( true );
+	}
+}
