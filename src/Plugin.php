c30bc37a9554d0a38cde1512418cf0eb7d6f2cc2
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-02-13 15:30:58 +0100

    Covered all code of Filters

--- src/Plugin.php
+++ src/Plugin.php
@@ -68,13 +68,7 @@ class Plugin {
 		define( 'OMGF_CACHE_IS_STALE', esc_attr( OMGF::get_option( Settings::OMGF_CACHE_IS_STALE ) ) );
 		define( 'OMGF_CURRENT_DB_VERSION', esc_attr( OMGF::get_option( Settings::OMGF_CURRENT_DB_VERSION ) ) );
 		define( 'OMGF_UPLOAD_DIR', apply_filters( 'omgf_upload_dir', WP_CONTENT_DIR . '/uploads/omgf' ) );
-		define(
-			'OMGF_UPLOAD_URL',
-			apply_filters(
-				'omgf_upload_url',
-				str_replace( [ 'http:', 'https:' ], '', WP_CONTENT_URL . '/uploads/omgf' )
-			)
-		);
+		define( 'OMGF_UPLOAD_URL', apply_filters( 'omgf_upload_url', str_replace( [ 'http:', 'https:' ], '', WP_CONTENT_URL . '/uploads/omgf' ) ) );
 	}
 
 	/**
