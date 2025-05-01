83538748184350c1c54a12e4031cb4c9df075238
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-05-01 17:26:59 +0200

    Clarified plugin's functions.

--- src/Admin/Settings/Optimize.php
+++ src/Admin/Settings/Optimize.php
@@ -74,7 +74,7 @@ class Optimize extends Builder {
 			<?php echo apply_filters(
 				'omgf_dashboard_intro',
 				__(
-					'OMGF replaces Google Fonts stylesheets (e.g. https://fonts.googleapis.com/css?family=Open+Sans) with locally hosted copies. To remove/unload Google Fonts entirely or by style/weight, go to <a href="#omgf-manage-optimized-fonts">Optimize Local Fonts</a>.',
+					'OMGF (Optimize My Google Fonts) automatically replaces Google Fonts stylesheets (e.g. https://fonts.googleapis.com/css?family=Open+Sans) with locally hosted copies. To remove/unload Google Fonts entirely or by style/weight, go to <a href="#omgf-manage-optimized-fonts">Optimize Local Fonts</a>.',
 					'host-webfonts-local'
 				),
 			); ?>
