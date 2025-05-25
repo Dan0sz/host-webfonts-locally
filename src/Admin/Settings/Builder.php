dbdc5f379d6ea85f12f20c3e115ee2db589bccc8
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-05-25 22:05:25 +0200

    Improved: prevent type coercion.

--- src/Admin/Settings/Builder.php
+++ src/Admin/Settings/Builder.php
@@ -159,7 +159,7 @@ class Builder {
 						if ( is_array( $selected ) ) {
 							$is_selected = in_array( $option, $selected );
 						} else {
-							$is_selected = $selected == $option;
+							$is_selected = $selected === $option;
 						}
 						?>
 						<option value="<?php echo esc_attr( $option ); ?>" <?php echo esc_attr(
