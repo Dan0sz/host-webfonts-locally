e5c77a3897e76bf63c9d5e501337fd948f04696f
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-06-11 15:08:52 +0200

    Fixed: tests.

--- tests/integration/API/AdminbarMenuTest.php
+++ tests/integration/API/AdminbarMenuTest.php
@@ -17,18 +17,20 @@ class AdminbarMenuTest extends TestCase {
 	 */
 	public function testGetAdminBarStatus() {
 		// We send over 1 URL, and it should be saved.
-		try {
-			$_POST[ 'path' ] = '/test';
-			$_POST[ 'urls' ] = [ 'https://fonts.googleapis.com/css?family=Roboto:400,700' ];
-			$api             = new AdminbarMenu();
+		$request = new \WP_REST_Request( 'POST', '/omgf/v1/adminbar-menu/status' );
 
-			$api->get_admin_bar_status();
+		try {
+			$request->set_param( 'path', '/test' );
+			$request->set_param( 'urls', [ 'https://fonts.googleapis.com/css?family=Roboto:400,700' ] );
+			$api = new AdminbarMenu();
+
+			$api->get_admin_bar_status( $request );
 
 			$results = OMGF::get_option( Settings::OMGF_GOOGLE_FONTS_CHECKER_RESULTS );
 
 			$this->assertArrayHasKey( 'https://fonts.googleapis.com/css?family=Roboto:400,700', $results );
 		} finally {
-			$_POST[ 'urls' ] = [];
+			$request->set_param( 'urls', [] );
 		}
 
 		// We send over no URLs and no URLs should be saved.
@@ -37,43 +39,40 @@ class AdminbarMenuTest extends TestCase {
 
 			$api = new AdminbarMenu();
 
-			$api->get_admin_bar_status();
+			$api->get_admin_bar_status( $request );
 
 			$results = OMGF::get_option( Settings::OMGF_GOOGLE_FONTS_CHECKER_RESULTS );
 
 			$this->assertArrayNotHasKey( 'https://fonts.googleapis.com/css?family=Roboto:400,700', $results );
 		} finally {
 			remove_filter( 'omgf_is_running_optimize', '__return_true' );
-
-			unset( $_POST[ 'urls' ] );
 		}
 
 		// We send over 6 URLs, but only 5 should be saved.
 		try {
 			add_filter( 'omgf_is_running_optimize', '__return_true' );
 
-			$_POST[ 'urls' ] = [
-				'https://fonts.googleapis.com/css?family=Test',
-				'https://fonts.googleapis.com/css?family=Test2',
-				'https://fonts.googleapis.com/css?family=Test3',
-				'https://fonts.googleapis.com/css?family=Test4',
-				'https://fonts.googleapis.com/css?family=Test5',
-				'https://fonts.googleapis.com/css?family=Test6',
-			];
+			$request->set_param(
+				'urls',
+				[
+					'https://fonts.googleapis.com/css?family=Test',
+					'https://fonts.googleapis.com/css?family=Test2',
+					'https://fonts.googleapis.com/css?family=Test3',
+					'https://fonts.googleapis.com/css?family=Test4',
+					'https://fonts.googleapis.com/css?family=Test5',
+					'https://fonts.googleapis.com/css?family=Test6',
+				]
+			);
 
 			$api = new AdminbarMenu();
 
-			$api->get_admin_bar_status();
+			$api->get_admin_bar_status( $request );
 
 			$results = OMGF::get_option( Settings::OMGF_GOOGLE_FONTS_CHECKER_RESULTS );
 
 			$this->assertCount( 5, $results );
 		} finally {
 			remove_filter( 'omgf_is_running_optimize', '__return_true' );
-
-			unset( $_POST[ '_wpnonce' ] );
-			unset( $_POST[ 'path' ] );
-			unset( $_POST[ 'urls' ] );
 
 			OMGF::delete_option( Settings::OMGF_GOOGLE_FONTS_CHECKER_RESULTS );
 		}
