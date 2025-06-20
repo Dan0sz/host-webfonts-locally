6ae8d354a43e150c28dd499faae9a0f3843d4b84
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-06-20 11:06:47 +0200

    Typo.

--- src/Plugin.php
+++ src/Plugin.php
@@ -63,7 +63,7 @@ class Plugin {
 			return;
 		}
 
-		/** Prevents undefined constant in OMGF Pro, if its not at version v3.3.0 (yet) */
+		/** Prevents undefined constant errors in OMGF Pro if it's not at version v3.3.0 (yet) */
 		define( 'OMGF_OPTIMIZATION_MODE', false );
 		define( 'OMGF_SITE_URL', 'https://daan.dev' );
 		define( 'OMGF_CACHE_IS_STALE', esc_attr( OMGF::get_option( Settings::OMGF_CACHE_IS_STALE ) ) );
