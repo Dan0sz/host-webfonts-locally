7a748fc5e2f534cf8ccecf43e565f496d58c868b
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-10-29 16:36:17 +0100

    Updated PHP Doc.

--- src/Frontend/Filters.php
+++ src/Frontend/Filters.php
@@ -28,7 +28,7 @@ class Filters {
 	 * @since  v5.3.3 Decode HTML entities to prevent URL decoding issues on some systems.
 	 *
 	 * @since  v5.4.3 With encoded URLs the Google Fonts API is much more lenient when it comes to invalid requests,
-	 *               but we need the URL to be decoded in order to properly parsed (parse_str() and parse_url()), etc.
+	 *               but we need the URL to be decoded to properly parse (parse_str() and parse_url()), etc.
 	 *               So, as of now, we're trimming invalid characters from the end of the URL. The list will expand
 	 *               as I run into to them. I'm not going to make any assumptions on what theme/plugin developers
 	 *               might be doing wrong.
@@ -46,7 +46,7 @@ class Filters {
 		$url = html_entity_decode( $url );
 
 		/**
-		 * Fix invalid query parameters. Mistakes made often by other theme/plugin devs when concatenating.
+		 * Fix invalid concatenation.
 		 */
 		$url = str_replace( [ ',&', '&,' ], '&', $url );
 		$url = str_replace( [ ',=', '=,' ], '=', $url );
