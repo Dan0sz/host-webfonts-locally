3c47586fcf9d36434ecf14be5c1553e2c5653bc0
Daan van den Bergh <18595395+Dan0sz@users.noreply.github.com>
2026-01-05 14:36:41 +0100

    Improved: description of Developer Mode (Pro)

--- src/Admin/Settings/Advanced.php
+++ src/Admin/Settings/Advanced.php
@@ -96,7 +96,7 @@ class Advanced extends Builder {
 			'omgf_developer_mode_description',
 			sprintf(
 				__(
-					'Enable this option (on all instances) if you\'re planning to use %s in a (variation of a) Development > Testing > Acceptance/Staging > Production workflow. %s',
+					'Enable this option (on all instances) to make it easier to migrate between Development/Testing/Staging/Production environments. %s',
 					'host-webfonts-local'
 				),
 				apply_filters( 'omgf_settings_page_title', 'OMGF' ),
