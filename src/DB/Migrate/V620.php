88d6d15cdfb0a9e31810d4112c5c5fe17d815a75
Daan van den Bergh <18595395+Dan0sz@users.noreply.github.com>
2026-03-15 14:42:26 +0100

    Added: migration script for 6.2.0.

---
+++ src/DB/Migrate/V620.php
@@ -0,0 +1,52 @@
+<?php
+
+/* * * * * * * * * * * * * * * * * * * * *
+*
+*  ██████╗ ███╗   ███╗ ██████╗ ███████╗
+* ██╔═══██╗████╗ ████║██╔════╝ ██╔════╝
+* ██║   ██║██╔████╔██║██║  ███╗█████╗
+* ██║   ██║██║╚██╔╝██║██║   ██║██╔══╝
+* ╚██████╔╝██║ ╚═╝ ██║╚██████╔╝██║
+*  ╚═════╝ ╚═╝     ╚═╝ ╚═════╝ ╚═╝
+*
+* @package  : OMGF
+* @author   : Daan van den Bergh
+* @copyright: © 2025 Daan van den Bergh
+* @url      : https://daan.dev
+* * * * * * * * * * * * * * * * * * * */
+
+namespace DB\Migrate;
+
+use OMGF\Admin\Settings;
+use OMGF\Helper;
+use OMGF\Helper as OMGF;
+
+/**
+ * @codeCoverageIgnore
+ */
+class V620 {
+	/** @var $version string The version number this migration script was introduced with. */
+	private $version = '6.2.0';
+
+	/**
+	 * Build class.
+	 */
+	public function __construct() {
+		$this->init();
+	}
+
+	/**
+	 * This migration script doesn't do much, besides showing a notice after updating.
+	 *
+	 * @return void
+	 */
+	private function init() {
+		Helper::delete_option( 'omgf_optimize_has_run' );
+		Helper::delete_option( 'auto_subsets' );
+
+		/**
+		 * Update stored version number.
+		 */
+		OMGF::update_option( Settings::OMGF_CURRENT_DB_VERSION, $this->version );
+	}
+}
